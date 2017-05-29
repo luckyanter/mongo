393a4f055a3c652f789556d5232e7eac1b4a7bc6
luckyanter <ahmedrezk@Ahmeds-MacBook-Air.local>
2017-05-29 15:25:50 -0400

    ll

---
+++ server.js
@@ -0,0 +1,200 @@
+/* Showing Mongoose's "Populated" Method (18.3.8)
+ * INSTRUCTOR ONLY
+ * =============================================== */
+
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/homework");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+
+// Routes www.nytimes.com   www.echojs.com
+// ======
+
+// A GET request to scrape the echojs website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("http://www.nytimes.com/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+//example of HTML a tag: <a href="https://www.w3schools.com">Visit W3Schools.com!</a>
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {//this will save entry to articles collection in database
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  // res.send("Scrape Complete");
+  res.redirect('/');
+});
+
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function(req, res) {
+  // Grab every doc in the Articles array
+  Article.find({}, function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Or send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+// Grab an article by its ObjectId
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.findOne({ "_id": req.params.id })
+  // ..and populate all of the notes associated with it
+  .populate("note")
+  // now, execute our query
+  .exec(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+
+// Create a new note or replace an existing note
+app.post("/articles/:id", function(req, res) {//this id belongs to specific article
+  // Create a new note and pass the req.body to the entry
+  var newNote = new Note(req.body);
+
+  // And save the new note the db
+  newNote.save(function(error, doc) {//this will save newNote in notes collection in database
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise
+    else {
+      // Use the article id to find and update its note
+      //the following code will save newNote or update the existing note with newNote in
+      //articles collections in database
+      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+      // Execute the above query
+      .exec(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        else {
+          // Or send the document to the browser
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
+
+app.post("/articles/delete/:id", function(req, res) {//this id belongs to specific article
+  // Create a new note and pass the req.body to the entry
+  var currentNote = new Note(req.body);
+  // var currentNote = req.body;
+  // currentNote.empty();
+
+  // And save the new note the db
+  // currentNote.delete(function(error, doc) {
+  currentNote.remove(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise
+    else {
+      // Use the article id to find and update its note
+      //the following code will save newNote or update the existing note with newNote in
+      //articles collections in database
+      Article.findOneAndRemove({ "_id": req.params.id }, { "note": doc._id })
+      // Execute the above query
+      .exec(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        else {
+          // Or send the document to the browser
+          res.send(doc);
+        }
+      });
+
+      
+
+       // Note.findOneAndRemove({ "note": doc._id }).exec(function(err, doc) {
+       //  if (err) {console.log(err);}});
+
+
+    }
+  });
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
